1775b63105c60cc2581fc0b7531fe372d754f4e0
Ross Terker <ross.terker@gmail.com>
2024-02-24 18:07:22 -0500

    feat: front end upload working with multiple. need to get server.js to handle the multiple files we are sending

--- Client/Components/App.jsx
+++ Client/Components/App.jsx
@@ -15,8 +15,20 @@ export default function App() {
     }
 
     const formData = new FormData();
-    formData.append('file', file);
+    // formData.append('file', file);
     
+    for (let i = 0; i < file.length; i++) {
+      // The first parameter is the name of the form field (used by the server to retrieve the uploaded file(s))
+      // The second parameter is the file object
+      // The third parameter is optional and represents the filename; if omitted, the File object's name is used
+      // Loop through the FileList and append each file to the FormData object
+      formData.append('file', file[i], file[i].originalname);
+    }
+    for (const pair of formData.entries()) {
+      console.log(pair[0], pair[1]);
+    }
+
+
     try {
       const response = await fetch('/api/fileupload', {
         method: 'POST',
