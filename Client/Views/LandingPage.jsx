832d61ed34ebf5ec27b916b925b06d3c41b06b1d
Scott Zembsch <scottzembsch@gmail.com>
2024-03-05 19:00:12 -0800

    app structure done

---
+++ Client/Views/LandingPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function LandingPage() {
+  return (
+  <>
+    <div>LandingPage</div>
+    <h1>hello</h1>
+    <a href="/signup">signup</a>
+    <a href="/login">login</a>
+  </>
+  )
+}
+
+export default LandingPage
--- Client/Views/UserMainPage.jsx
+++ Client/Views/UserMainPage.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import React from 'react';
+
+import RepoUpload from '../Components/RepoUpload.jsx';
+import D3 from '../Components/D3.jsx';
 
 function UserMainPage() {
   return (
+  <>
     <div>UserMainPage</div>
+    <RepoUpload />
+    <D3 />
+  </>
   )
 }
 
