7eb9301d0ae389b086531279bf8d5a69d71e321b
Scott Zembsch <scottzembsch@gmail.com>
2024-03-12 11:23:46 -0700

    landing page almost done, chipping on meet team section

---
+++ Client/Components/Shatter.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Spline from '@splinetool/react-spline';
+
+
+function Shatter() {
+  return (
+    // <Spline scene="https://prod.spline.design/hyCP58S8Opt6Z7tC/scene.splinecode" />
+    <Spline scene="https://prod.spline.design/oP2MdamIGTutKUSr/scene.splinecode" />
+  )
+}
+
+export default Shatter
--- Client/Views/LandingPage.jsx
+++ Client/Views/LandingPage.jsx
@@ -2,13 +2,17 @@ import React from 'react'
 import '../Styles/landingpage.css';
 
 import screenshot from '../Assets/Screenshot 2024-03-07 at 9.21.34 PM.png';
-import blocks1 from "../Assets/blocks1.png";
 import Header from '../Components/Header.jsx';
+import Blocks from '../Components/blocks.jsx';
+import Shatter from '../Components/Shatter.jsx';
+import scott from '../Assets/teamPics/scottimg.jpg';
+
 
 function LandingPage() {
   return ( 
   <>
     <Header />
+
     <div className='landingMasterDiv'>
       <div className='main-top-container'>
         <div className='greeting-text'>Microservice Migration Simplified</div>
@@ -17,7 +21,14 @@ function LandingPage() {
           <button className='btn btn-focus'>Download</button>
           <button className='btn btn-secondary'>Sign Up</button>
         </div>
+        <div className='shatter'>
+          <Shatter />
+        </div>
       </div>
+
+
+
+
       <div className='main-second-container'>
         <div className='main-s-inner'>
           <img className='app-img1' src={screenshot} alt="" /> 
@@ -29,34 +40,43 @@ function LandingPage() {
       <div className='hz-rule'></div>
       
       <div className='main-third-container'>
-        <div>
-          {/* <img className='blocksimg' src={blocks1} alt="" /> */}
+
+        <div className='mt-l-container'>
+          <div className='mt-l-text'>
+          With MonoSplits data driven approach, you are able to see potential microservices in your codebase.
+          </div>
+
+        </div>
+        <div className='tc-inner-container'>
+        <Blocks />
         </div>
       </div>
 
       <div className='hz-rule'></div>
-      <div>
+
+
+      <div className='fourth-container-main'>
         <div className='meet-header'>Meet The Team</div>
         <div className='team-container'>
           <div className='person-card'>
-            <img src="" alt="" />
-            <div>Ross Terker</div>
+            <img className='person-img' src="" alt="" />
+            <div className='person-name'>Ross Terker</div>
           </div>
           <div className='person-card'>
-            <img src="" alt="" />
-            <div>Moises Gomez</div>
+            <img className='person-img' src="" alt="" />
+            <div className='person-name'>Moises Gomez</div>
           </div>
           <div className='person-card'>
-            <img src="" alt="" />
-            <div>Scott Zembsch</div>
+            <img className='person-img' src={scott} alt="" />
+            <div className='person-name'>Scott Zembsch</div>
           </div>
           <div className='person-card'>
-            <img src="" alt="" />
-            <div>Greg Silvia</div>
+            <img className='person-img' src="" alt="" />
+            <div className='person-name'>Greg Silvia</div>
           </div>
           <div className='person-card'>
-            <img src="" alt="" />
-            <div>Christian Magorrian</div>
+            <img className='person-img' src="" alt="" />
+            <div className='person-name'>Christian Magorrian</div>
           </div>
         </div>
       </div>
