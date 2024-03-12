7ac82040808ec50bf39d4b71e48156fb4c9484e7
Scott Zembsch <scottzembsch@gmail.com>
2024-03-11 19:50:21 -0700

    chipping on landing page

--- Client/Views/LandingPage.jsx
+++ Client/Views/LandingPage.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import '../Styles/landingpage.css';
-import screenshot from '../Assets/Screenshot 2024-03-07 at 9.21.34 PM.png';
 
+import screenshot from '../Assets/Screenshot 2024-03-07 at 9.21.34 PM.png';
+import blocks1 from "../Assets/blocks1.png";
 import Header from '../Components/Header.jsx';
 
 function LandingPage() {
@@ -21,10 +22,48 @@ function LandingPage() {
         <div className='main-s-inner'>
           <img className='app-img1' src={screenshot} alt="" /> 
           <div className='ms-r-container'>
-            <div className='ms-r-text'>text about what the user can do in our app, all the visual tools and the heiarchy of the code base</div>
+            <div className='ms-r-text'>With Fisson, you can visualize your code structure in an highly informative and digestable manner with total control over what data you need. View file heiarchy, dependencies, or even code complextiy with the click of a button. </div>
           </div>
         </div>
       </div>
+      <div className='hz-rule'></div>
+      
+      <div className='main-third-container'>
+        <div>
+          {/* <img className='blocksimg' src={blocks1} alt="" /> */}
+        </div>
+      </div>
+
+      <div className='hz-rule'></div>
+      <div>
+        <div className='meet-header'>Meet The Team</div>
+        <div className='team-container'>
+          <div className='person-card'>
+            <img src="" alt="" />
+            <div>Ross Terker</div>
+          </div>
+          <div className='person-card'>
+            <img src="" alt="" />
+            <div>Moises Gomez</div>
+          </div>
+          <div className='person-card'>
+            <img src="" alt="" />
+            <div>Scott Zembsch</div>
+          </div>
+          <div className='person-card'>
+            <img src="" alt="" />
+            <div>Greg Silvia</div>
+          </div>
+          <div className='person-card'>
+            <img src="" alt="" />
+            <div>Christian Magorrian</div>
+          </div>
+        </div>
+      </div>
+      <div className='hz-rule'></div>
+      <footer className='main-foot'>
+        <div>Contact</div>
+      </footer>
     </div>
   </>
   )
