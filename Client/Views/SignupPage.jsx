13826060ba2a42266ad0f6ba36972e11f2758dde
Scott Zembsch <scottzembsch@gmail.com>
2024-03-13 00:05:21 -0700

    chipping on signup page

--- Client/Views/SignupPage.jsx
+++ Client/Views/SignupPage.jsx
@@ -34,35 +34,37 @@ function SignupPage() {
 
   return (
     <>
-    <div>
-      <div className='enter-username'>
-        <label className='input-title'>Username:</label>
-        <input type="text" name='username' value={formData.username} onChange={handleChange}/>
-        
+    <div className='signup-container'>
+      <div className='form-container'>
+        <div className='enter-username'>
+          <label className='input-title'>Username:</label>
+          <input type="text" name='username' value={formData.username} onChange={handleChange}/>
+          
+        </div>
+        <div className='enter-firstName'>
+          <label>First name:</label>
+          <input type="text" name='firstName' value={formData.firstName} onChange={handleChange}/>
+          
+        </div>
+        <div className='enter-lastName'>
+          <label>Last name:</label>
+          <input type="text" name='lastName' value={formData.lastName} onChange={handleChange}/>
+          
+        </div>
+        <div className='enter-email'>
+          <label>Email:</label>
+          <input type="text" name='email' value={formData.email} onChange={handleChange}/>
+          
+        </div>
+        <div className='enter-password'>
+          <label>Password:</label>
+          <input type="password" name='password' value={formData.password} onChange={handleChange} />
+          
+        </div>
+        <input type="button" value='Submit' onClick={handleSubmit}/>
       </div>
-      <div className='enter-firstName'>
-        <label>First name:</label>
-        <input type="text" name='firstName' value={formData.firstName} onChange={handleChange}/>
-        
-      </div>
-      <div className='enter-lastName'>
-        <label>Last name:</label>
-        <input type="text" name='lastName' value={formData.lastName} onChange={handleChange}/>
-        
-      </div>
-      <div className='enter-email'>
-        <label>Email:</label>
-        <input type="text" name='email' value={formData.email} onChange={handleChange}/>
-        
-      </div>
-      <div className='enter-password'>
-        <label>Password:</label>
-        <input type="password" name='password' value={formData.password} onChange={handleChange} />
-        
-      </div>
-      <input type="button" value='Submit' onClick={handleSubmit}/>
-    </div>
     <img className='bg-img' src={bgIMG} alt="" />
+    </div>
 
     </>
   )
