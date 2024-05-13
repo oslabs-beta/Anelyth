f280caa69628d143ed8230cc54cc3d73272506ea
Ross Terker <ross.terker@gmail.com>
2024-05-12 22:13:07 -0400

    feat: handling template literals in api query controller refactor: appending separate objects for each type of ast node

--- src/Server/Controllers/AstApiQueryController2.mjs
+++ src/Server/Controllers/AstApiQueryController2.mjs
@@ -1,5 +1,6 @@
 import fs from 'fs';
 import ApiAnalyzer from '../utils/ApiAnalyzer.mjs';
+import chalk from 'chalk';
 
 const AstApiQueryController2 = {};
 
@@ -9,13 +10,13 @@ AstApiQueryController2.queryFunc = async (nodeAST, nodePath) => {
   console.log('\n');
   console.log('Inside AstApiQueryController2.queryFunc')
   console.log(`analyzing file path ${nodePath}`);
-  const apiAnalyzer = new ApiAnalyzer();
   try {
+    const apiAnalyzer = new ApiAnalyzer();
     apiAnalyzer.setApiType('native');
-    const nativeMatches = apiAnalyzer.getApiCalls(nodeAST, ['fetch']);
+    apiAnalyzer.getApiCalls(nodeAST, ['fetch']);
     apiAnalyzer.setApiType('imported');
-    const importedMatches = apiAnalyzer.getApiCalls(nodeAST, ['axios']);
-    const result = [...nativeMatches, ...importedMatches];
+    apiAnalyzer.getApiCalls(nodeAST, ['axios']);
+    const result = apiAnalyzer.getMatches();
     console.log('result from getApiCalls: ', result);
     if (result.length === 0) {
       return ({
@@ -24,15 +25,15 @@ AstApiQueryController2.queryFunc = async (nodeAST, nodePath) => {
         details: []
       });
     } else {
-      const stream = fs.createWriteStream('./api-query-testing-nodes.log');
+      const stream = fs.createWriteStream('./api-query-testing-nodes.log', {flags: 'a'});
       stream.write(JSON.stringify(result, null, 2));
       stream.end();
       return analyze(result, nodePath);
     }
 
   } catch (err) {
-    console.error('Error in AstApiQueryController2.queryFunc:', err);
-    throw new Error(`'Error in AstApiQueryController2.queryFunc: ${err}`);
+    console.error(chalk.red(`Error in AstApiQueryController2.queryFunc: ${err}`));
+    // throw new Error(`'Error in AstApiQueryController2.queryFunc: ${err}`);
     // return ({
     //   log: 'error in AstApiQueryController2.queryFunc',
     //   message: err,
@@ -42,34 +43,29 @@ AstApiQueryController2.queryFunc = async (nodeAST, nodePath) => {
 
 //input: apiInteractions (object where each key is an api and the value is an object with numNodes and nodes) and filePath
 //output: object with filePath, totalInteractions number, details: array of Objects (each object refers to an api)
-//TODO: need to handle template literals. argument value does not include the variable passed in to it. if we just want the url domain, 
-//can grab that from the argument value, unless it's passed in; then you'd have to jump through more hoops to determine what the url is
+//TODO: we may want to grab just the url domain. this works fine for strings, but we need to handle cases for template literals where the url value is stored in a variable
 function analyze(apiInteractions, filePath) {
   console.log(`\x1b[35mInside Fetch API Extended Analysis`);
   console.log('apiInteractions:', apiInteractions)
 
   let totalInteractions = 0;
-  // // let apiArguments = {};
-  // for (const api of apiInteractions) {
   const details = apiInteractions.map((api) => {
     //add up total api calls in each file
     const numApiInteractions = api.numNodes;
     if (numApiInteractions > 0) {
       totalInteractions += numApiInteractions;
     }
-    console.log('Api:', api.apiName)
-    console.log('Element in object: ', api);
     //get arguments (data endpoints for each api call)
     let nodes = api.nodes;
     const args = [];
     nodes.forEach((node, index) => {
       console.log(`node ${index + 1} arguments: ${node.arguments}`);
 
-      //TODO: need to account for imported apis, where there is not arguments property directly on the node. need to grab the call expression then grab the arguments
+      //TODO: need to account for imported apis
       for (let i = 0; i < node.arguments?.length; i++) {
         const arg = node.arguments[i];
         console.log('Argument type: ', arg.type);
-        let argValue;
+        let argValue = '';
         switch (arg.type) {
           case 'Literal':
             console.log('Literal arg value: ', arg.value);
@@ -79,15 +75,25 @@ function analyze(apiInteractions, filePath) {
             console.log('Identifier arg name: ', arg.name);
             argValue = arg.name;
             break;
-          //TODO: handle all parts of template literal. just returning the first section of the string, ignoring the rest. Console logs can guide you.
           case 'TemplateLiteral':
             console.log('Template Literal Parts:');
-            arg.quasis.forEach((part, index) => console.log(`Text part ${index + 1} value:`, part.value.cooked));
-            arg.expressions.forEach((expr, index) => {
-                console.log(`Expression ${index + 1} type: ${expr.type}`);
-                console.log(`Expression ${index + 1} name: ${expr.name}`);
+            const quasisEls = arg.quasis.map(({ start, value }) => {
+              return ({
+                start,
+                value: value.cooked
+              });
             });
-            argValue = arg.quasis[0].value.cooked;
+            //name is the variable name for the reference
+            const expressionEls = arg.expressions.map(({ start, name }) => {
+              return ({
+                start,
+                value: name
+              });
+            });
+            const els = quasisEls.concat(expressionEls).sort((a, b) => a.start - b.start);
+            els.forEach(({ value }) => {
+              argValue += value;
+            });
             break;
           default:
             console.log('Argument is not a Literal, Identifier, or TemplateLiteral and was not handled in ApiQueryController');
@@ -102,13 +108,7 @@ function analyze(apiInteractions, filePath) {
       endpoints: args
     });
   });
-  const stream = fs.createWriteStream('./api-query-testing.log');
-
-  // const memberExpressionFetchCalls = esquery.query(ast, 'CallExpression[callee.type="MemberExpression"][callee.property.name="fetch"]');
-  // const regularFunctionFetchCalls = esquery.query(ast,'CallExpression[callee.type="Identifier"][callee.name="fetch"]');
-
-  // const regularFunctionRequireCalls = esquery.query(ast,'CallExpression[callee.type="Identifier"][callee.name="require"]');
-  // const memberExpressionRequireCalls = esquery.query(ast, 'CallExpression[callee.type="MemberExpression"][callee.property.name="require"]');
+  const stream = fs.createWriteStream('./api-query-testing.log', {flags: 'a'});
 
   const fileDetails = {
     filePath,
--- src/Server/utils/ApiAnalyzer.mjs
+++ src/Server/utils/ApiAnalyzer.mjs
@@ -1,4 +1,5 @@
 import esquery from 'esquery';
+import chalk from 'chalk';
 
 class ApiAnalyzer {
   constructor() {
@@ -6,10 +7,16 @@ class ApiAnalyzer {
     this.type = null;
     this.currQuery = null;
     this.args = null;
+    this.importRefs = {};
+    this.matches = [];
   }
 
   setApiType(type) {
     return this.type = type;
+  }
+
+  getMatches() {
+    return this.matches;
   }
 
   setQuery(strings, api) {
@@ -24,7 +31,7 @@ class ApiAnalyzer {
     this.currQuery = result;
   }
 
-  getNodes(ast, api) {
+  getNodes(ast, type, api) {
     const nodes = esquery.query(
       ast,
       this.currQuery
@@ -36,19 +43,19 @@ class ApiAnalyzer {
 
     return ({
       apiName: api,
+      type,
       numNodes: nodes.length,
       nodes
     });
   }
 
-  findApiCalls(ast, apiLibraries) {
+  getApiCalls(ast, apiLibraries) {
     if (!ast || !apiLibraries) {
       return console.log('Please pass in ast and apiLibraries');
     }
-    const matches = [];
     const libraries = [...apiLibraries];
 
-    if (type === 'imported') {
+    if (this.type === 'imported') {
       //This list should eventually be a list of all imported api's we can handle. we can then have the user pass in the api libraries they are using in their codebase, which would
       //correspond to the apiLibraries arg
       //add libraries you want to check for HERE
@@ -59,44 +66,48 @@ class ApiAnalyzer {
         for (let j = 0; j < libraries.length; j++) {
           const currLibrary = libraries[j].toLowerCase();
           if (currLibrary === currApi) {
-            this.setQuery`VariableDeclarator[init.callee.name="require"][init.arguments.0.value="${currApi}"],
-            CallExpression[callee.name="require"][arguments.0.value="${currApi}"],
-            ImportDeclaration[source.value="${currApi}"]`;
-            const result = this.getNodes(ast, currApi);
-            if (result) {
-              //TODO: get variable being assigned, then look up call expressions based on that name
-              matches.push(result);
+            // this.setQuery`VariableDeclarator[init.callee.name="require"][init.arguments.0.value="${currApi}"]`;
+            // const requireVariableDeclarators = this.getNodes(ast, 'requireVariableDeclarators', currApi);
+            this.setQuery`CallExpression[callee.name="require"][arguments.0.value="${currApi}"]`; 
+            const requireCallExpressions = this.getNodes(ast, 'requireCallExpressions', currApi);
+            this.setQuery`ImportDeclaration[source.value="${currApi}"]`;
+            const importDeclarations = this.getNodes(ast, 'importDeclarations', currApi);
+            if (requireCallExpressions || importDeclarations) {
+              this.matches.push(requireCallExpressions, importDeclarations);
             }
             libraries.splice(j, 1);
           }
         }
         i++;
       }
-    } else if (type === 'native') {
+    } else if (this.type === 'native') {
       // Special checks for APIs that don't require import/require in a browser environment
       // const browserAPIs = ['fetch', 'XMLHttpRequest', '$', 'jQuery'];
       //This list should eventually be a list of all native api's we can handle. we can then have the user pass in the api libraries they are using in their codebase, which would
       //correspond to the apiLibraries arg
       //add libraries you want to check for HERE
       const nativeApis = ['fetch'];
-      let k = 0;
-      while (k < nativeApis.length && libraries.length > 0) {
-        const currApi = nativeApis[k].toLowerCase();
+      let i = 0;
+      while (i < nativeApis.length && libraries.length > 0) {
+        const currApi = nativeApis[i].toLowerCase();
         for (let j = 0; j < libraries.length; j++) {
           const currLibrary = libraries[j].toLowerCase();
           if (currLibrary === currApi) {
             this.setQuery`CallExpression[callee.type="MemberExpression"][callee.property.name="${currApi}"], CallExpression[callee.type="Identifier"][callee.name="${currApi}"]`;
-            const result = this.getNodes(ast, currApi);
-            if (result) {
-              matches.push(result);
+            const nativeCallExpressions = this.getNodes(ast, 'nativeCallExpressions', currApi);
+            if (nativeCallExpressions) {
+              this.matches.push(nativeCallExpressions);
             }
             libraries.splice(j, 1);
           }
         }
-        k++;
+        i++;
       }
     }
-    return matches;
+  }
+
+  setImportRefs() {
+    
   }
 }
 
