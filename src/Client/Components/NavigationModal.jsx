9431eb6c4394fc102913c03789ba9130e1d50575
Moises Gomez <moisesgomez@Juanas-Air.lan>
2024-05-12 21:34:34 -0700

    lost all previous commit history but essentially this is commit recovers all the work done for the documentation

---
+++ src/Client/Components/NavigationModal.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const NavigationModal = () => {
+    return (<div className='navigationModal'>
+        <h3>GET STARTED</h3>
+        <ul>
+        <li><a href="#installation">Installation</a></li>
+        <li><a href="#usage">How to Use</a></li>
+        <li><a href="#user-main">User Main Page Description</a></li>
+        <li><a href="#projects">Projects Page Description</a></li>
+        <li><a href="#save-ast">Saving AST</a></li>
+        <li><a href="#save-visual">Saving Visual</a></li>
+        <li><a href="#limitations">Limitations</a></li>
+        </ul>
+        </div>);
+}
+
+export default NavigationModal;
