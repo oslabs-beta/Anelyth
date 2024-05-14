8d11cfaf5d64a5f042edc578d7e00404613ee405
Ross Terker <ross.terker@gmail.com>
2024-05-13 22:57:58 -0400

    refactor: passing in the ast to class constructor

--- src/Server/Controllers/AstApiQueryController2.mjs
+++ src/Server/Controllers/AstApiQueryController2.mjs
@@ -12,21 +12,19 @@ AstApiQueryController2.queryFunc = async (nodeAST, nodePath) => {
   console.log(`analyzing file path ${nodePath}`);
   try {
     const stream = fs.createWriteStream('./api-query-testing-nodes.log', {flags: 'a'});
-    const nativeApiAnalyzer = new NativeApiAnalyzer();
-    nativeApiAnalyzer.getApiCalls(nodeAST, ['fetch']);
+    const nativeApiAnalyzer = new NativeApiAnalyzer(nodeAST);
+    nativeApiAnalyzer.getApiCalls(['fetch']);
     nativeApiAnalyzer.setArgs();
-    const importedApiAnalyzer = new ImportedApiAnalyzer();
-    importedApiAnalyzer.getApiCalls(nodeAST, ['axios']);
+    const importedApiAnalyzer = new ImportedApiAnalyzer(nodeAST);
+    importedApiAnalyzer.getApiCalls(['axios']);
     importedApiAnalyzer.setImportRefs();
-    importedApiAnalyzer.setArgs(nodeAST);
+    importedApiAnalyzer.setArgs();
+    const importedApiAnalyzerResult = importedApiAnalyzer.getMatches();
+    const nativeApiAnalyzerResult = nativeApiAnalyzer.getMatches();
     stream.write(JSON.stringify(nativeApiAnalyzer.args, null, 2));
     stream.write(JSON.stringify(importedApiAnalyzer.refs, null, 2));
     stream.write(JSON.stringify(importedApiAnalyzer.args, null, 2));
     stream.end();
-    const importedApiAnalyzerResult = importedApiAnalyzer.getMatches();
-    const nativeApiAnalyzerResult = nativeApiAnalyzer.getMatches();
-    console.log('result from importedApiAnalyzer: ', importedApiAnalyzerResult);
-    console.log('result from nativeApiAnalyzer: ', nativeApiAnalyzerResult);
     const result = [...importedApiAnalyzerResult, ...nativeApiAnalyzerResult];
     if (result.length === 0) {
       return ({
@@ -35,9 +33,32 @@ AstApiQueryController2.queryFunc = async (nodeAST, nodePath) => {
         details: []
       });
     } else {
-      return analyze(result, nodePath);
+      console.log(`\x1b[35mInside Fetch API Extended Analysis`);
+      console.log('apiInteractions:', apiInteractions)
+    
+      let totalInteractions = 0;
+      const numApiInteractions = api.numNodes;
+      if (numApiInteractions > 0) {
+        totalInteractions += numApiInteractions;
+      }
+      // const details = [{
+      //   api: api.apiName,
+      //   numInteractions: api.numNodes,
+      //   endpoints: args
+      // }]
+      const stream = fs.createWriteStream('./api-query-testing.log', {flags: 'a'});
+    
+      const fileDetails = {
+        filePath,
+        totalInteractions,
+        details
+      };
+    
+      stream.write(JSON.stringify(fileDetails, null, 2));
+      stream.end();
+    
+      return fileDetails;
     }
-
   } catch (err) {
     console.error(chalk.red(`Error in AstApiQueryController2.queryFunc: ${err}`));
     // throw new Error(`'Error in AstApiQueryController2.queryFunc: ${err}`);
@@ -52,81 +73,7 @@ AstApiQueryController2.queryFunc = async (nodeAST, nodePath) => {
 //output: object with filePath, totalInteractions number, details: array of Objects (each object refers to an api)
 //TODO: we may want to grab just the url domain. this works fine for strings, but we need to handle cases for template literals where the url value is stored in a variable
 function analyze(apiInteractions, filePath) {
-  console.log(`\x1b[35mInside Fetch API Extended Analysis`);
-  console.log('apiInteractions:', apiInteractions)
 
-  let totalInteractions = 0;
-  const details = apiInteractions.map((api) => {
-    //add up total api calls in each file
-    const numApiInteractions = api.numNodes;
-    if (numApiInteractions > 0) {
-      totalInteractions += numApiInteractions;
-    }
-    //get arguments (data endpoints for each api call)
-    let nodes = api.nodes;
-    const args = [];
-    nodes.forEach((node, index) => {
-      console.log(`node ${index + 1} arguments: ${node.arguments}`);
-
-      //TODO: need to account for imported apis
-      for (let i = 0; i < node.arguments?.length; i++) {
-        const arg = node.arguments[i];
-        console.log('Argument type: ', arg.type);
-        let argValue = '';
-        switch (arg.type) {
-          case 'Literal':
-            console.log('Literal arg value: ', arg.value);
-            argValue = arg.value;
-            break;
-          case 'Identifier': 
-            console.log('Identifier arg name: ', arg.name);
-            argValue = arg.name;
-            break;
-          case 'TemplateLiteral':
-            console.log('Template Literal Parts:');
-            const quasisEls = arg.quasis.map(({ start, value }) => {
-              return ({
-                start,
-                value: value.cooked
-              });
-            });
-            //name is the variable name for the reference
-            const expressionEls = arg.expressions.map(({ start, name }) => {
-              return ({
-                start,
-                value: name
-              });
-            });
-            const els = quasisEls.concat(expressionEls).sort((a, b) => a.start - b.start);
-            els.forEach(({ value }) => {
-              argValue += value;
-            });
-            break;
-          default:
-            console.log('Argument is not a Literal, Identifier, or TemplateLiteral and was not handled in ApiQueryController');
-            break;
-        }
-        args.push(argValue);
-      }
-    });
-    return ({
-      api: api.apiName,
-      numInteractions: api.numNodes,
-      endpoints: args
-    });
-  });
-  const stream = fs.createWriteStream('./api-query-testing.log', {flags: 'a'});
-
-  const fileDetails = {
-    filePath,
-    totalInteractions,
-    details
-  };
-
-  stream.write(JSON.stringify(fileDetails, null, 2));
-  stream.end();
-
-  return fileDetails;
 
 }
 
--- src/Server/utils/ApiAnalyzer.mjs
+++ src/Server/utils/ApiAnalyzer.mjs
@@ -2,7 +2,8 @@ import esquery from 'esquery';
 import chalk from 'chalk';
 
 class Analyzer {
-  constructor() {
+  constructor(ast) {
+    this.ast = ast;
     this.currQuery = null;
     this.args = [];
     this.matches = [];
@@ -24,9 +25,9 @@ class Analyzer {
     this.currQuery = result;
   }
 
-  getNodes(ast, type, api) {
+  getNodes(type, api) {
     const nodes = esquery.query(
-      ast,
+      this.ast,
       this.currQuery
     );
 
@@ -41,75 +42,18 @@ class Analyzer {
       nodes
     });
   }
-
-  // getApiCalls(ast, apiLibraries) {
-  //   if (!ast || !apiLibraries) {
-  //     return console.log('Please pass in ast and apiLibraries');
-  //   }
-  //   const libraries = [...apiLibraries];
-
-  //   if (this.type === 'imported') {
-  //     //This list should eventually be a list of all imported api's we can handle. we can then have the user pass in the api libraries they are using in their codebase, which would
-  //     //correspond to the apiLibraries arg
-  //     //add libraries you want to check for HERE
-  //     const importedApis = ['axios'];
-  //     let i = 0;
-  //     while (i < importedApis.length && libraries.length > 0) {
-  //       const currApi = importedApis[i].toLowerCase();
-  //       for (let j = 0; j < libraries.length; j++) {
-  //         const currLibrary = libraries[j].toLowerCase();
-  //         if (currLibrary === currApi) {
-  //           // this.setQuery`VariableDeclarator[init.callee.name="require"][init.arguments.0.value="${currApi}"]`;
-  //           // const requireVariableDeclarators = this.getNodes(ast, 'requireVariableDeclarators', currApi);
-  //           this.setQuery`CallExpression[callee.name="require"][arguments.0.value="${currApi}"]`; 
-  //           const requireCallExpressions = this.getNodes(ast, 'requireCallExpressions', currApi);
-  //           this.setQuery`ImportDeclaration[source.value="${currApi}"]`;
-  //           const importDeclarations = this.getNodes(ast, 'importDeclarations', currApi);
-  //           if (requireCallExpressions || importDeclarations) {
-  //             this.matches.push(requireCallExpressions, importDeclarations);
-  //           }
-  //           libraries.splice(j, 1);
-  //         }
-  //       }
-  //       i++;
-  //     }
-  //   } else if (this.type === 'native') {
-  //     // Special checks for APIs that don't require import/require in a browser environment
-  //     // const browserAPIs = ['fetch', 'XMLHttpRequest', '$', 'jQuery'];
-  //     //This list should eventually be a list of all native api's we can handle. we can then have the user pass in the api libraries they are using in their codebase, which would
-  //     //correspond to the apiLibraries arg
-  //     //add libraries you want to check for HERE
-  //     const nativeApis = ['fetch'];
-  //     let i = 0;
-  //     while (i < nativeApis.length && libraries.length > 0) {
-  //       const currApi = nativeApis[i].toLowerCase();
-  //       for (let j = 0; j < libraries.length; j++) {
-  //         const currLibrary = libraries[j].toLowerCase();
-  //         if (currLibrary === currApi) {
-  //           this.setQuery`CallExpression[callee.type="MemberExpression"][callee.property.name="${currApi}"], CallExpression[callee.type="Identifier"][callee.name="${currApi}"]`;
-  //           const nativeCallExpressions = this.getNodes(ast, 'nativeCallExpressions', currApi);
-  //           if (nativeCallExpressions) {
-  //             this.matches.push(nativeCallExpressions);
-  //           }
-  //           libraries.splice(j, 1);
-  //         }
-  //       }
-  //       i++;
-  //     }
-  //   }
-  // }
 }
 
 
 class ImportedApiAnalyzer extends Analyzer {
-  constructor() {
-    super();
+  constructor(ast) {
+    super(ast);
     this.refs = {};
   }
 
-  getApiCalls(ast, apiLibraries) {
-    if (!ast || !apiLibraries) {
-      return console.log('Please pass in ast and apiLibraries');
+  getApiCalls(apiLibraries) {
+    if (!apiLibraries) {
+      return console.log('Please pass in apiLibraries');
     }
     const libraries = [...apiLibraries];
     //This list should eventually be a list of all imported api's we can handle. we can then have the user pass in the api libraries they are using in their codebase, which would
@@ -123,11 +67,11 @@ class ImportedApiAnalyzer extends Analyzer {
         const currLibrary = libraries[j].toLowerCase();
         if (currLibrary === currApi) {
           this.setQuery`VariableDeclarator[init.callee.name="require"][init.arguments.0.value="${currApi}"]`;
-          const requireVariableDeclarators = this.getNodes(ast, 'requireVariableDeclarators', currApi);
+          const requireVariableDeclarators = this.getNodes('requireVariableDeclarators', currApi);
           this.setQuery`CallExpression[callee.name="require"][arguments.0.value="${currApi}"]`; 
-          const requireCallExpressions = this.getNodes(ast, 'requireCallExpressions', currApi);
+          const requireCallExpressions = this.getNodes('requireCallExpressions', currApi);
           this.setQuery`ImportDeclaration[source.value="${currApi}"]`;
-          const importDeclarations = this.getNodes(ast, 'importDeclarations', currApi);
+          const importDeclarations = this.getNodes('importDeclarations', currApi);
           if (requireVariableDeclarators || requireCallExpressions || importDeclarations) {
             this.matches.push(requireVariableDeclarators, requireCallExpressions, importDeclarations);
           }
@@ -166,7 +110,7 @@ class ImportedApiAnalyzer extends Analyzer {
     });
   }
 
-  setArgs(ast) {
+  setArgs() {
     if (Object.keys(this.refs).length === 0) {
       console.log('There are 0 importRefs. This could be for 2 reasons: 1) there are no importRefs in the current file 2) you need to populate imporRefs.' +
       'Make sure you populate importRefs first by invoking setImportRefs. Then invoke getArgs.');
@@ -174,7 +118,7 @@ class ImportedApiAnalyzer extends Analyzer {
     for (let key in this.refs) {
       this.refs[key].forEach((ref) => {
         this.setQuery`CallExpression[callee.type="MemberExpression"][callee.property.name="${ref}"], CallExpression[callee.type="Identifier"][callee.name="${ref}"]`;
-        const importRefCallNodes = this.getNodes(ast, 'importRefCallExpressions');
+        const importRefCallNodes = this.getNodes('importRefCallExpressions');
         if (importRefCallNodes) {
           //get arguments (data endpoints for each api call)
           let nodes = importRefCallNodes.nodes;
@@ -227,13 +171,13 @@ class ImportedApiAnalyzer extends Analyzer {
 }
 
 class NativeApiAnalyzer extends Analyzer {
-  constructor() {
-    super();
+  constructor(ast) {
+    super(ast);
   }
   
-  getApiCalls(ast, apiLibraries) {
-    if (!ast || !apiLibraries) {
-      return console.log('Please pass in ast and apiLibraries');
+  getApiCalls(apiLibraries) {
+    if (!apiLibraries) {
+      return console.log('Please pass in apiLibraries');
     }
     const libraries = [...apiLibraries];
     // Special checks for APIs that don't require import/require in a browser environment
@@ -249,7 +193,7 @@ class NativeApiAnalyzer extends Analyzer {
         const currLibrary = libraries[j].toLowerCase();
         if (currLibrary === currApi) {
           this.setQuery`CallExpression[callee.type="MemberExpression"][callee.property.name="${currApi}"], CallExpression[callee.type="Identifier"][callee.name="${currApi}"]`;
-          const nativeCallExpressions = this.getNodes(ast, 'nativeCallExpressions', currApi);
+          const nativeCallExpressions = this.getNodes('nativeCallExpressions', currApi);
           if (nativeCallExpressions) {
             this.matches.push(nativeCallExpressions);
           }
